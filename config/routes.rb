2c5bef526247bf122ad6eee02723825a44670e1c
Muhammed MUsa <72840992+Galapagous@users.noreply.github.com>
2023-05-08 09:22:25 +0100

    adding admin previledge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :projects
+      resources :users
+
+      root to: "projects#index"
+    end
   devise_for :users
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
